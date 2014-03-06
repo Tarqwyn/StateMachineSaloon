7be6fe6f3fd3afed0355c4580b0e7d5949cbb0ee
Richard Bangay <richard.bangay@gmail.com>
2014-03-06 02:04:36 +0000

    added all the new portrait and landscape breakpoint images

---
+++ assets/js/bootstrap.js
@@ -0,0 +1,20 @@
+// Set of dependencies
+// This gives us flexibility to change dependencies with a common API e.g. Bonzo --> jQuery
+// JQuery and IStats delivered via BBC require Map on all BBC responsive pages...
+// Here we could define other dependencies that may be require specifically by our application
+// for example rapheal or a pubsub module..
+define([
+    // '../../lib/jquery',
+    '../../lib/event_emitter'
+], function (
+    // jquery,
+    EventEmitter
+) {
+    var news = {
+        // $: jquery,
+        $: $,
+        pubsub: new EventEmitter()
+    };
+
+    return news;
+});
