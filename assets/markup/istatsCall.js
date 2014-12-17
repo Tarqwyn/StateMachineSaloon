d9397d4bd008e76619c480934f6722b3ecc06ebe
Tarqwyn <steven@tarquindandy.com>
2014-12-17 11:53:45 +0000

    video refactor

--- assets/markup/istatsCall.js
+++ assets/markup/istatsCall.js
@@ -1,28 +1,35 @@
 (function () {
-	'use strict';
-	$(function () {
-		var currentSectionNumber;
-		$('.section').each(function (i) {
-			$(this).scrollWatch().on('scroll', function (e) {
-				if (Math.abs(e.visibility) > .8) {
-					if (currentSectionNumber !== i) {
-					currentSectionNumber = i;
-						try {
-							istatsGlobal.log(
-								'section-view',
-								'newsspec-nonuser',
-								{
-									'time':Date.now(),
-									'section': i
-								}
-							);
-						}
-						catch(e) {
-							console.log('istats error, error:', e);
-						}
-					}
-				}
-			})
-		})
-	})
-})();
+    'use strict';
+    $(function () {
+        var sections = [];
+        var currentSectionNumber;
+        $.each($('.section-navigator'), 
+            function (i, value) {
+                $('div[id$=' + ($(value).attr('href').split('-')[1]) + ']').scrollWatch().on('scrollin', function (e) {
+                    if (Math.abs(e.visibility) > .8) {
+                        if (currentSectionNumber !== i) {
+                        currentSectionNumber = i;
+                            try {
+                                if (typeof istatsGlobal !== 'undefined') {
+                                    if (sections.indexOf(i + 1) === -1) {
+                                    istatsGlobal.log(
+                                        'section-view',
+                                        'newsspec-nonuser',
+                                        {
+                                            'time':Date.now(),
+                                            'section': i + 1
+                                        }
+                                    );
+                                    sections.push(i + 1)
+                                    }
+                                }
+                            }
+                            catch(e) {
+                                console.log('istats error, error:', e);
+                            }
+                        }
+                    }
+                })
+            })
+        })
+    })();
