24b4e246704874ec9c0d0bf8f52a8447a8997039
Nzar Tofiq <nzar.tofiq@bbc.co.uk>
2015-02-16 12:00:36 +0000

    footer link colour change from blue to red

--- assets/markup/shareTools.js
+++ assets/markup/shareTools.js
@@ -25,9 +25,9 @@ var _initialiseModule = function () {
 };
 
 model = new NSShareModel({
-    message: "Arms wide open: The story behind Rio's Christ the Redeemer statue",
+    message: "Chilling story - the icy ingenuity of the men who changed the planet forever by cooling us down #BBCRicherWorld - http://www.bbc.co.uk/news/magazine-31149328",
     desc: "Shared via BBC News Magazine",
-    image: "http://www.bbc.co.uk/news/special/2014/newsspec_6954/static/img/mbaye225.jpg"
+    image: "http://news.bbcimg.co.uk/news/special/2015/newsspec_10193/media/share_image.jpg"
 });
 model.storyPageUrl = window.document.location.href;
 view = new NSShareView('#ns_share_module');
